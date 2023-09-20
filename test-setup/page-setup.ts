9dd53a0bba516909bdb3faeb15068e3c37b6bbe0
vasu31dev <vasu31dev@gmail.com>
2023-09-20 15:22:19 -0400

    Updated the page-setup to export expect as well.

--- test-setup/page-setup.ts
+++ test-setup/page-setup.ts
@@ -1,17 +1,17 @@
 /**
- * page-setup.ts: This module is responsible for setting up the initial state of a page before each test.
- * It includes a hook that runs before each test, setting the page context. By centralizing these setup operations,
- * it ensures a consistent starting point for each test, improving test reliability. It also exports a base test object
- * with a beforeEach hook already set up. This can be used to define tests with the page context set up.
+ * Page-setup.ts: This module is responsible for setting up the initial state of a page before each test. It includes a
+ * hook that runs before each test, setting the page context. By centralizing these setup operations, it ensures a
+ * consistent starting point for each test, improving test reliability. It also exports a base test object with a
+ * beforeEach hook already set up. This can be used to define tests with the page context set up.
  */
 
-import { test as baseTest } from '@playwright/test';
+import { test as baseTest, expect } from '@playwright/test';
 import { setPage } from 'vasu-playwright-utils';
 
 /**
- * A hook that runs before each test, setting the page context.
- * The base test object with a beforeEach hook is already set up.
- * This can be used to define tests with the page context set up.
+ * A hook that runs before each test, setting the page context. The base test object with a beforeEach hook is already
+ * set up. This can be used to define tests with the page context set up.
+ *
  * @param {Page} page - The page context provided by Playwright.
  */
 export const test = baseTest.extend<{ testHook: void }>({
@@ -25,3 +25,5 @@ export const test = baseTest.extend<{ testHook: void }>({
     { auto: true },
   ],
 });
+
+export { expect };
