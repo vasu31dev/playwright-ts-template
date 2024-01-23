0e0f6ee66ed65984110e724fb90091b8d3368b37
vasu31dev <vasu31dev@gmail.com>
2024-01-23 10:10:24 -0500

    Updated with dynamic xpath for  addToCartButton

--- tests/pages/sauce-demo/sauce-demo-products-page.ts
+++ tests/pages/sauce-demo/sauce-demo-products-page.ts
@@ -3,7 +3,8 @@ import { expectElementToBeHidden, expectElementToBeVisible } from 'vasu-playwrig
 import { getLocator } from 'vasu-playwright-utils';
 
 const productsContainer = () => getLocator(`#inventory_container`).nth(0);
-const addToCartButton = `(//*[@class='inventory_item'])[%s]//*[contains(@id,'add-to-cart')]`;
+// Defining a dynamic xpath based on the position of the Add to cart element
+const addToCartButton = (num: number) => `(//*[@class='inventory_item'])[${num}]//*[contains(@id,'add-to-cart')]`;
 
 export async function verifyProductsPageIsDisplayed() {
   await expectElementToBeVisible(productsContainer(), { timeout: 1000, message: 'Logged in user should see Products' });
@@ -14,5 +15,5 @@ export async function verifyProductsPageIsNotDisplayed() {
 }
 
 export async function addToCartByProductNumber(productNo: number) {
-  await click(addToCartButton.replace('%s', productNo.toString()));
+  await click(addToCartButton(productNo));
 }
