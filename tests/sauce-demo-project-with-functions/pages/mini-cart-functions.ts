86165c19adb8aef9196a5a8761655eda8e6f96e9
vasu31dev <vasu31dev@gmail.com>
2024-03-25 21:23:55 -0400

    Remove mini-cart-functions example

--- tests/sauce-demo-project-with-functions/pages/mini-cart-functions.ts
+++
@@ -1,8 +0,0 @@
-import { expectElementToHaveText } from 'vasu-playwright-utils';
-import { getLocator } from 'vasu-playwright-utils';
-
-const miniCartCount = () => getLocator(`//*[@id='shopping_cart_container']//span`);
-
-export async function verifyMiniCartCount(expectedMiniCartCount: string) {
-  await expectElementToHaveText(miniCartCount(), expectedMiniCartCount);
-}
